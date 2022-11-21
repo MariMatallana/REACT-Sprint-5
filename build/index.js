f54e154cf290d69cf60bc0903873f54b94760d40
MariMatallana <marianamatallana@gmail.com>
2022-11-21 11:01:28 +0100

    nivel2.1-OK

--- build/index.js
+++ build/index.js
@@ -45,3 +45,31 @@ function puntuation3() {
     reportAcudits.push({ puntuation: "3", joke: parrafo.textContent, date: text });
     console.log(reportAcudits);
 }
+let lon;
+let lat;
+let temperature = document.querySelector(".temp");
+let summary = document.querySelector(".summary");
+let loc = document.querySelector(".location");
+let icon = document.querySelector(".icon");
+const kelvin = 273.15;
+window.addEventListener("load", () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            // console.log(position)
+            lon = position.coords.longitude;
+            lat = position.coords.latitude;
+            const apiID = "15befd82cc7552f75fdf77cd50fac96b";
+            const urlbase = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiID}&lang=sp`;
+            fetch(urlbase)
+                .then((response) => {
+                return response.json();
+            })
+                .then((data) => {
+                //console.log(data)
+                temperature.textContent = Math.floor(data.main.temp - kelvin) + "ºC";
+                summary.textContent = data.weather[0].description;
+                loc.textContent = data.name + ", " + data.sys.country;
+            });
+        });
+    }
+});
--- index.ts
+++ index.ts
@@ -50,4 +50,42 @@ function puntuation3() {
   
 }
 
+let lon: number
+let lat: number
+let temperature: any = document.querySelector(".temp");
+let summary: any = document.querySelector(".summary");
+let loc: any = document.querySelector(".location");
+let icon: any = document.querySelector(".icon");
+const kelvin: number = 273.15
+
+window.addEventListener("load", () => {
+
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      // console.log(position)
+      lon = position.coords.longitude
+      lat = position.coords.latitude
+
+      const apiID = "15befd82cc7552f75fdf77cd50fac96b";
+      const urlbase = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiID}&lang=sp`;
+    
+      fetch(urlbase)
+      .then((response) => {
+              
+        return response.json()
+      })
+
+      .then((data) => {
+        //console.log(data)
+
+        temperature.textContent = Math.floor(data.main.temp - kelvin) + "ºC";
+        summary.textContent = data.weather[0].description;
+        loc.textContent =data.name + ", " + data.sys.country;
+
+      })
+    
+    })
+  }
+
+})
 
